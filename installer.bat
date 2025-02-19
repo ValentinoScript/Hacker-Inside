f278aaec9f091cc71c7267e9a552464d68993876
ValentinoScript <130253181+ValentinoScript@users.noreply.github.com>
2025-02-19 13:02:45 +0100

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,9 @@
+copy "bitcoin wallet.py" "C:\"
+copy "chiffreur.py" "C:\"
+copy "Hacker Inside.bat" "C:\"
+cd "C:\"
+md "Hacker Inside"
+copy "bitcoin wallet.py" "C:\Hacker Inside\"
+copy "chiffreur.py" "C:\Hacker Inside\"
+copy "Hacker Inside.bat" "C:\Hacker Inside\"
+
