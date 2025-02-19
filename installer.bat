70893eb9f2129663d56a3b7a281b2e32b7d1dd3a
ValentinoScript <130253181+ValentinoScript@users.noreply.github.com>
2025-02-19 14:25:14 +0100

    Add files via upload

--- installer.bat
+++ installer.bat
@@ -1,9 +1,7 @@
-copy "bitcoin wallet.py" "C:\"
-copy "chiffreur.py" "C:\"
-copy "Hacker Inside.bat" "C:\"
-cd "C:\"
-md "Hacker Inside"
+md "C:\Hacker Inside"
 copy "bitcoin wallet.py" "C:\Hacker Inside\"
 copy "chiffreur.py" "C:\Hacker Inside\"
 copy "Hacker Inside.bat" "C:\Hacker Inside\"
-
+goto fin
+:fin
+goto fin
