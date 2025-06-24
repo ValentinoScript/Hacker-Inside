d351deac313836c5f8240077d5fa1db24c009f6c
ValentinoScript <130253181+ValentinoScript@users.noreply.github.com>
2025-06-24 08:59:11 +0200

    Update installer (executez le en administrateur et en premier.bat

--- installer (executez le en administrateur et en premier.bat
+++ installer (executez le en administrateur et en premier.bat
@@ -1,4 +1,14 @@
+@echo off
+If EXIST "C:\Hacker Inside" rd "C:\Hacker Inside"
 md "C:\Hacker Inside"
 copy "bitcoin wallet.py" "C:\Hacker Inside\"
 copy "chiffreur.py" "C:\Hacker Inside\"
 copy "Hacker Inside.bat" "C:\Hacker Inside\"
+copy "wallet.py" "C:\Hacker Inside"
+copy "wallet_test3.py" "C:\Hacker Inside"
+copy "Logo.ico" "C:\Hacker Inside"
+python "Logo Creator.py"
+echo Installation terminé, vous pouvez fermer cette fenettre
+goto fin
+:fin
+goto fin
