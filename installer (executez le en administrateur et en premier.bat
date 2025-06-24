c98a816651a9b25996464f16f4cbc420c469cb3c
ValentinoScript <130253181+ValentinoScript@users.noreply.github.com>
2025-06-24 09:18:11 +0200

    Update installer (executez le en administrateur et en premier.bat

--- installer (executez le en administrateur et en premier.bat
+++ installer (executez le en administrateur et en premier.bat
@@ -7,6 +7,10 @@ copy "Hacker Inside.bat" "C:\Hacker Inside\"
 copy "wallet.py" "C:\Hacker Inside"
 copy "wallet_test3.py" "C:\Hacker Inside"
 copy "Logo.ico" "C:\Hacker Inside"
+echo python est nécéssaire au fonctionnement de l'application, voulez-vous l'installer ? (O/N)
+set /p "cho=>"
+IF %cho% EQU O python
+IF %cho% EQU o python
 python "Logo Creator.py"
 echo Installation terminé, vous pouvez fermer cette fenettre
 goto fin
